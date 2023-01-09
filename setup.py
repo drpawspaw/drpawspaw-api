7749442d51eea2a9799e02ac54d36340abf3b192
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2023-01-09 15:43:22 +0000

    Modularize functions and fix issues

---
+++ setup.py
@@ -0,0 +1,7 @@
+import nltk
+
+# Download required NLTK libraries and files
+nltk.download('wordnet')  # get synonyms
+nltk.download('omw-1.4')
+nltk.download('stopwords')  # stop words
+nltk.download('punkt')  # tokenizer
