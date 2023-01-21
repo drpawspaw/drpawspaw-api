8512cbbca62ef9361566d7b2f94aea96a3969b85
Viraj Lakshitha Bandara <virajlakshithabandara@gmail.com>
2023-01-21 18:51:47 +0530

    Implement base functionalities include mongodb connection, authentication

---
+++ models/user/handler.py
@@ -0,0 +1,23 @@
+from marshmallow import Schema, fields
+import datetime as dt
+import uuid
+
+class User(object):
+    def __init__(self, name, email):
+        self._id = uuid.uuid4()
+        self.name = name
+        self.email = email
+        self.provider = provider
+        self.image_url = image_url
+        self.created_at = dt.datetime.now()
+
+    def __repr__(self):
+        return '<User(name={self.name!r})>'.format(self=self)
+
+class UserSchema(Schema):
+    name = fields.Str()
+    provider = fields.Str()
+    image_url = fields.Str()
+    email = fields.Email()
+    created_at = fields.Date()
+    type = fields.Str()
