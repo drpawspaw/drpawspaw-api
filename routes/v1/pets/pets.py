1857e76d6e41f2796f88e16f97c4605f8aca183f
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2023-03-18 14:27:31 +0000

    Implement sendgrid email service

--- routes/v1/pets/pets.py
+++ routes/v1/pets/pets.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
-from models.pets.handler import PetSchema
+from models.pets.handler import PetSchema, VaccineEmailSchema
 from routes import database_conn
+from utils.email_sender.handler import send_reminder_email
 from bson.objectid import ObjectId
 import datetime
 
@@ -31,4 +32,13 @@ def pets():
             return jsonify(pets), 200
         return { "data": "Unable to retreive the data" }, 500
 
-        
+@pet_api.route("/vaccine-reminder", methods=['POST'])
+def send_reminder():
+    email_req = VaccineEmailSchema().load(request.get_json())
+    return send_reminder_email(
+        email_req['email'], 
+        email_req['pet'], 
+        email_req['owner'], 
+        email_req['vaccine'], 
+        email_req['date'].strftime("%d %b, %Y")
+    )
