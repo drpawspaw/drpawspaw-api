462c292fd911b6b98288811343eb86d87d516453
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2023-03-18 08:52:10 +0000

    Implement APIs to create and read pet profiles

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # drpawspaw-api
 REST API service for drpawspaw.com
+
+## CURL Requests
+
+### Pets
+
+1. Create pet's profile
+```
+curl --location 'http://localhost:8000/api/v1/pets' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "Tyson",
+    "birthdate": "2023-02-02T06:50:04.254+00:00",
+    "lastVaccination": "Parvo",
+    "lastVaccinationDate": "2023-03-22T06:50:04.254+00:00",
+    "bread": "Shitzu",
+    "category": "dog"
+}'
+```
---
+++ models/pets/handler.py
@@ -0,0 +1,25 @@
+from marshmallow import Schema, fields
+import datetime as df
+import uuid
+
+class Pet(object):
+    def __init__(self, name, birthdate, lastVaccination, lastVaccinationDate, category, bread, owner):
+        self._id = uuid.uuid4()
+        self.name = name
+        self.birthdate = birthdate
+        self.lastVaccination = lastVaccination
+        self.lastVaccinationDate = lastVaccinationDate
+        self.category = category
+        self.bread = bread
+        self.owner = owner
+        self.created_at = df.datetime.now()
+
+class PetSchema(Schema):
+    name = fields.Str()
+    birthdate = fields.DateTime()
+    lastVaccination = fields.Str()
+    lastVaccinationDate = fields.DateTime()
+    category = fields.Str()
+    bread = fields.Str()
+    owner = fields.Str()
+    created_at = fields.DateTime()
