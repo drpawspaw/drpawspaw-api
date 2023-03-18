7c604b4251ede7158fbab567ad2f4e457d1a5deb
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2023-03-18 06:25:30 +0000

    Add DialogFlow webhook authentication

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_cors import CORS
 from routes.auth.auth import auth_api
 from routes.v1.users.users import user_api
+from routes.v1.chatbot.chatbot import chatbot_api
 
 # load environment variables
 load_dotenv()
@@ -18,6 +19,7 @@ CORS(app)
 # define module routes
 app.register_blueprint(user_api)
 app.register_blueprint(auth_api)
+app.register_blueprint(chatbot_api)
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
