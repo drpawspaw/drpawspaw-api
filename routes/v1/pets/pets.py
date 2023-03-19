1a770ba72e8d17d83f6d7fb2fb36edd39a3df9b5
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2023-03-19 04:15:35 +0000

    Complete the pet profile APIs

--- routes/v1/pets/pets.py
+++ routes/v1/pets/pets.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from models.pets.handler import PetSchema, VaccineEmailSchema
+from models.pets.handler import PetSchema
 from routes import database_conn
 from utils.email_sender.handler import send_reminder_email
 from bson.objectid import ObjectId
@@ -10,35 +10,55 @@ pet_api = Blueprint("pets", __name__, url_prefix="/api/v1/pets")
 # define mongodb collection
 pet_collection = database_conn['pets']
 
-@pet_api.route('/', methods=['POST', 'GET'])
-def pets():
+@pet_api.route('/', methods=['POST', 'GET', 'PUT', 'DELETE'])
+def retrieve_create_pets():
     # Create new pet profile
     if request.method == 'POST':
         new_pet = PetSchema().load(request.get_json())
         pet_collection.insert_one(new_pet)
         return {  "data": "Record added successfully" }, 201
 
-    # Retreive all the pets
     if request.method == 'GET':
         try:
+            # Request pet by profile id
             req_pet = pet_collection.find_one({"_id": ObjectId(request.args['id'])})
             req_pet['_id'] = str(req_pet['_id'])
             return jsonify(req_pet), 200
         except:
-            pets = []
-            for pet in pet_collection.find():
-                pet['_id'] = str(pet['_id'])
-                pets.append(pet)
-            return jsonify(pets), 200
-        return { "data": "Unable to retreive the data" }, 500
+            try:
+                # Request pets by owner id
+                owner_pet = []
+                for pet in pet_collection.find():
+                    if pet['owner'] == request.args['owner']:
+                        pet['_id'] = str(pet['_id'])
+                        owner_pet.append(pet)
+                return jsonify(owner_pet), 200
+            except:
+                # Request all the pets
+                pets = []
+                for pet in pet_collection.find():
+                    pet['_id'] = str(pet['_id'])
+                    pets.append(pet)
+                return jsonify(pets), 200
+        return { "data": "Unable to retreive the records" }, 500
+    
+@pet_api.route('/<id>', methods=['PUT', 'DELETE'])
+def update_delete(id):
+    if request.method == 'PUT':
+        req_update = PetSchema().load(request.get_json())
+        result = pet_collection.find_one_and_update(
+            {"_id" : ObjectId(id)},
+            {"$set": req_update},
+            upsert=True
+        )
+        if result["_id"] == ObjectId(id):
+            return { "data": "Updated successfully" }, 200
+        return { "data": "Unable to update the record" }, 500
 
-@pet_api.route("/vaccine-reminder", methods=['POST'])
-def send_reminder():
-    email_req = VaccineEmailSchema().load(request.get_json())
-    return send_reminder_email(
-        email_req['email'], 
-        email_req['pet'], 
-        email_req['owner'], 
-        email_req['vaccine'], 
-        email_req['date'].strftime("%d %b, %Y")
-    )
+    if request.method == 'DELETE':
+        result = pet_collection.find_one_and_delete(
+            {"_id" : ObjectId(id)}
+        )
+        if result["_id"] == ObjectId(id):
+            return { "data": "Deleted successfully" }, 200
+        return { "data": "Unable to delete the record" }, 500
