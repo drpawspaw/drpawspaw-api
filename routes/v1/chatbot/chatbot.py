dadc4c02215d0e2ba1d85cca015535c8be870c19
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2023-03-23 01:25:01 +0000

    Add comments to make the code more readable

--- routes/v1/chatbot/chatbot.py
+++ routes/v1/chatbot/chatbot.py
@@ -18,6 +18,8 @@ def dialogflow_detect_intent():
     session = session_client.session_path(os.getenv("DIALOGFLOW_PROJECT"), req['session'])
     text_input = dialogflow.TextInput(text=req['message'], language_code='en-US')
     query_input = dialogflow.QueryInput(text=text_input)
+
+    # Get the intent detection and generate output from the DialogFlow
     result = session_client.detect_intent(request={"session": session, "query_input": query_input})
 
     response = {}
@@ -26,18 +28,23 @@ def dialogflow_detect_intent():
     response['intent'] = result.query_result.intent.display_name
     response['confidence'] = result.query_result.intent_detection_confidence
     response['type'] = "GENERAL"
+    response['suggestions'] = [] # By default this will be an empty array, To keep the response entity constant, 
 
-    # TODO - Handle disease part and update the dialogflow model
+    # If the intent recognize as "PREDICTION REQUEST", then pass the message to entity_linker for the prediction
     if result.query_result.intent.name == decision_request_intent:
         pred_resp = entity_linker(result.query_result.query_text)
-        print(pred_resp)
         response['type'] = pred_resp['result_type']
+
+        # Limitation 
         if pred_resp['result_type'] == 'LIMITATION':
             response['type'] = "LIMITATION"
             response['response'] = "We don't have the knowledge to figure out what disease it is based on the information we have."
+        # Suggestion - In here, user will get list of symptoms for the prediction.
         elif pred_resp['result_type'] == 'SUGGESTION':
             response['type'] = "SUGGESTION"
-            response['response'] = "According to the provided information we are not able to identify extact disease, But it may be {diseases}. If you can provide more symptoms out of follwoing systom, that would be help to perform the prediction more accurate. \n\n Suggestions: {symptoms}".format(diseases= ", ".join(pred_resp['predicted_disease']), symptoms= ", ".join(pred_resp['symptom_suggestions']))
+            response['response'] = "According to the provided information we are not able to identify extact disease, But it may be {diseases}. If you can provide more symptoms out of follwoing systom, that would be help to perform the prediction more accurate.".format(diseases= ", ".join(pred_resp['predicted_disease']))
+            response['suggestions'] = pred_resp['symptom_suggestions']
+        # Prediction
         else:
             response['response'] = "We are able to predict the disease as {disease}".format(disease= pred_resp['predicted_disease'])
             response['type'] = "PREDICTION"
